f3a8ee3190a2aa35bbd085ea1cc9e57fdabdebed
Ana Petrovic <petrovic.bs.ana@gmail.com>
2023-09-12 00:25:03 +0200

    Added material-ui

---
+++ client/theme.js
@@ -0,0 +1,24 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+import { blueGrey, lightGreen } from '@material-ui/core/colors'
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+      light: '#8eacbb',
+      main: '#013220',
+      dark: '#34515e',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#e7ff8c',
+      main: '#800080',
+      dark: '#7ecb20',
+      contrastText: '#000',
+    },
+      openTitle: blueGrey['400'],
+      protectedTitle: lightGreen['400'],
+      type: 'light'
+    }
+  })
+
+  export default theme
