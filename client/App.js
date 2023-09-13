606a27cf05e84a3a3606977a72d62cb874da50e4
Ana Petrovic <petrovic.bs.ana@gmail.com>
2023-09-13 16:27:56 +0200

    Implemented App.js

--- client/App.js
+++ client/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import MainRouter from './MainRouter'
+import {BrowserRouter} from 'react-router-dom'
+import { ThemeProvider } from '@material-ui/styles'
+import theme from './theme'
+import { hot } from 'react-hot-loader'
 
 const App = () => {
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles)
+    }
+  }, [])
   return (
-    <h1>Welcome To Proshop</h1>
-  )
-}
+  <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <MainRouter/>
+      </ThemeProvider>
+  </BrowserRouter>
+)}
 
-export default App
+export default hot(module)(App)
+
